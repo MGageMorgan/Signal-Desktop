9fa4e877ee65749997773fe4cd9ee552c710440f
lilia <liliakai@gmail.com>
2017-05-24 13:14:56 -0700

    Add database migration
    // FREEBIE

--- js/database.js
+++ js/database.js
@@ -217,6 +217,34 @@
                 });
                 next();
             }
+        },
+        {
+            version: "13.0",
+            migrate: function(transaction, next) {
+                console.log('migration 13.0');
+                console.log('Adding fields to identity keys');
+                var identityKeys = transaction.objectStore('identityKeys');
+                var request = identityKeys.openCursor();
+                request.onsuccess = function(event) {
+                  var cursor = event.target.result;
+                  if (cursor) {
+                    var attributes = cursor.value;
+                    attributes.timestamp = 0;
+                    attributes.firstUse = false;
+                    attributes.blockingApproval = true;
+                    attributes.nonblockingApproval = false;
+                    attributes.seen = 0;
+                    identityKeys.put(attributes, attributes.id);
+                    cursor.continue();
+                  } else {
+                    // no more results
+                    next();
+                  }
+                };
+                request.onerror = function(event) {
+                  console.log(event);
+                };
+            }
         }
     ];
 }());
